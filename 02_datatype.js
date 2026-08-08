6f8958f86e12f17a6c6f620ca411ebc15e9059f5
Your Name <neerajrajputmewada82@gmail.com>
2026-08-08 16:54:25 +0530

    02_datatype.js

---
+++ 02_datatype.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3) //we are using nodejs , browser
+
+console.log(3
+     + 3) //code readability should be high
+
+console.log("Neeraj")
+
+let name = "neeraj"
+let age = 20
+let islogged = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => true/false
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
+
+
+
+
